4a6043a41ba372eaad573fd423e3eb7ff88a3919
reichard <reichardcris@gmail.com>
2025-11-25 16:31:48 +0800

    update + med,low,high

--- Esp32wroom-Hotspot/Esp32wroom-Hotspot.ino
+++ Esp32wroom-Hotspot/Esp32wroom-Hotspot.ino
@@ -2,9 +2,13 @@
 #include <ESPmDNS.h>
 #include <WebSocketsServer.h>
 #include <IRremote.hpp>
+#include <LiquidCrystal_I2C.h>
 
 // Hotspot credentials
 const char* ssid = "ESP32-Hotspot";
+
+// LCD display settings
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 const int IR_SEND_PIN = 4;
 
@@ -38,36 +42,50 @@ uint16_t rawDataOff[83] = {
   550
 };
 
-// This is the data for the "Down" key (Command 0x0D)
-// Generated from the query data (0x08 0x1D 0x08 0x04 0x0C) by changing command to 0x0D
-uint16_t rawDataDown[83] = {
+// This is the data for the "Low" setting
+uint16_t rawDataLow[83] = {
   8900,4550,
-  500,550, 550,550, 500,600, 500,1650,
-  500,600, 500,550, 550,550, 500,550,
-  550,550, 500,1650, 550,1650, 500,1650,
-  500,1650, 550,550, 500,600, 500,550,
-  550,550, 500,600, 450,600, 550,1650,
-  500,550, 550,550, 500,600, 450,600,
-  500,600, 500,600, 500,550, 500,600,
-  500,1650, 500,600, 500,550, 550,550,
-  500,600, 500,550, 550,1650, 500,1650,
-  500,600, 500,550, 550,550, 500,600,
-  500
+  450,600, 500,600, 500,600, 500,1650,
+  500,600, 450,600, 550,550, 500,600,
+  450,1700, 500,1650, 500,1700, 500,1650,
+  500,1650, 550,550, 500,600, 450,600,
+  500,600, 500,600, 500,550, 500,1700,
+  450,650, 500,600, 450,600, 500,600,
+  500,550, 500,600, 500,600, 450,600,
+  500,1700, 500,600, 450,600, 500,600,
+  500,600, 450,600, 500,1650, 500,1700,
+  500,550, 550,550, 500,600, 500,550,
+  550
 };
 
-// This is the data for the "Up" key
-// Decoded from the new rawIRTimings provided
-uint16_t rawDataUp[83] = {
+// This is the data for the "Medium" setting
+uint16_t rawDataMedium[83] = {
   8950,4500,
-  500,600, 500,550, 550,550, 500,1650,
-  550,550, 500,600, 450,600, 550,550,
-  500,600, 500,1650, 500,1650, 550,1650,
-  500,1650, 500,600, 450,600, 550,550,
-  500,600, 500,550, 500,600, 500,1650,
+  500,550, 550,550, 500,600, 500,1650,
   500,600, 500,600, 500,550, 500,600,
-  500,600, 500,550, 500,600, 500,550,
+  500,1650, 550,1650, 500,550, 550,1650,
+  500,1650, 500,600, 500,550, 550,550,
+  500,600, 500,550, 550,550, 500,1650,
+  550,550, 500,600, 500,550, 550,550,
+  500,600, 500,550, 550,550, 500,600,
+  500,1650, 500,600, 500,550, 550,550,
+  500,600, 500,550, 550,1650, 500,1650,
+  500,550, 550,550, 500,600, 500,550,
+  550
+};
+
+// This is the data for the "High" setting
+uint16_t rawDataHigh[83] = {
+  8950,4500,
+  500,600, 500,600, 450,600, 500,1700,
+  450,600, 500,600, 500,600, 500,550,
   550,1650, 500,550, 550,550, 500,600,
-  500,550, 550,550, 500,1650, 550,1650,
+  500,1650, 500,600, 500,550, 550,550,
+  500,600, 500,550, 550,550, 500,1650,
+  550,550, 500,600, 500,550, 550,550,
+  500,600, 500,550, 500,600, 500,600,
+  500,1650, 500,600, 500,600, 500,550,
+  500,600, 500,600, 500,1650, 500,1650,
   500,550, 550,550, 500,600, 500,550,
   550
 };
@@ -78,33 +96,40 @@ WebSocketsServer webSocket = WebSocketsServer(8080);
 void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length) {
   Serial.println("WebSocket event received");
   switch(type) {
-      case WStype_DISCONNECTED:
-          Serial.printf("[%u] Disconnected!\n", num);
-          break;
-      case WStype_CONNECTED: {
-          IPAddress ip = webSocket.remoteIP(num);
-          Serial.printf("[%u] Connected from %d.%d.%d.%d url: %s\n", num, ip[0], ip[1], ip[2], ip[3], payload);
-          webSocket.sendTXT(num, "Connected to ESP32 Hotspot");
+    case WStype_DISCONNECTED:
+        Serial.printf("[%u] Disconnected!\n", num);
+        lcd.setCursor(0, 1);
+        lcd.print("Device disconnected");
+        break;
+    case WStype_CONNECTED: {
+        IPAddress ip = webSocket.remoteIP(num);
+        lcd.setCursor(0, 1);
+        lcd.print("Device connected");
+        Serial.printf("[%u] Connected from %d.%d.%d.%d url: %s\n", num, ip[0], ip[1], ip[2], ip[3], payload);
+        webSocket.sendTXT(num, "Connected to ESP32 Hotspot");
+    }
+        break;
+    case WStype_TEXT:
+      Serial.printf("[%u] get Text: %s\n", num, payload);
+      if (strcmp((char*)payload, "low") == 0) {
+        lcd.setCursor(0, 1);
+        lcd.print("Temp set to Low");
+        Serial.println("Low setting");
+        sendCommand(rawDataLow, sizeof(rawDataLow) / sizeof(rawDataLow[0]));
+      } else if (strcmp((char*)payload, "medium") == 0) {
+        Serial.println("Medium setting");
+        lcd.setCursor(0, 1);
+        lcd.print("Temp set to Medium");
+        sendCommand(rawDataMedium, sizeof(rawDataMedium) / sizeof(rawDataMedium[0]));
+      } else if (strcmp((char*)payload, "high") == 0) {
+        Serial.println("High setting");
+        lcd.setCursor(0, 1);
+        lcd.print("Temp set to High");
+        sendCommand(rawDataHigh, sizeof(rawDataHigh) / sizeof(rawDataHigh[0]));
       }
-          break;
-      case WStype_TEXT:
-          Serial.printf("[%u] get Text: %s\n", num, payload);
-          if (strcmp((char*)payload, "temp_up") == 0) {
-            Serial.println("Temperature up");
-            sendCommand(rawDataUp, sizeof(rawDataUp) / sizeof(rawDataUp[0]));
-          } else if (strcmp((char*)payload, "temp_down") == 0) {
-            sendCommand(rawDataDown, sizeof(rawDataDown) / sizeof(rawDataDown[0]));
-            Serial.println("Temperature down");
-          } else if (strcmp((char*)payload, "power_on") == 0) {
-            sendCommand(rawDataOn, sizeof(rawDataOn) / sizeof(rawDataOn[0]));
-            Serial.println("Power on");
-          } else if (strcmp((char*)payload, "power_off") == 0) {
-            sendCommand(rawDataOff, sizeof(rawDataOff) / sizeof(rawDataOff[0]));
-            Serial.println("Power off");
-          }
-          // Echo back the received message
-          webSocket.sendTXT(num, (char*)payload);
-          break;
+      // Echo back the received message
+      webSocket.sendTXT(num, (char*)payload);
+      break;
   }
 }
 
@@ -112,6 +137,14 @@ void setup() {
   Serial.begin(115200);
 
   IrSender.begin(IR_SEND_PIN);
+
+
+    // LCD init
+    lcd.init();
+    lcd.backlight();
+    lcd.setCursor(0, 0);
+    lcd.print("ESP32 Hotspot");
+
 
   // Start ESP32 in Access Point mode
   WiFi.softAP(ssid);
